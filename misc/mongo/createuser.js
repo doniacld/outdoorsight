09884939fa17b738da5736c4a66cf73235314861
Donia Chaiehloudj <donia.chaiehloudj.external@airbus.com>
2020-09-02 18:13:42 +0200

    Delete useless file

--- misc/mongo/createuser.js
+++
@@ -1,9 +0,0 @@
-// give all permissions to developer to connect
-db.createUser({
-    user: 'os',
-    pwd: 'os',
-    roles: [
-        {role: "userAdminAnyDatabase", db: "admin"},
-        {role: "readWriteAnyDatabase", db: "admin"},
-        {role: "dbAdminAnyDatabase", db: "admin"}, {role: "clusterAdmin", db: "admin"}]
-});
